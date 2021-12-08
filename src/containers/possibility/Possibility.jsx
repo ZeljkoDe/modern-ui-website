16cf53cedf836808da01a314b023bb73fbd408f4
Zeljko Damnjanovic <zeljko.de@outlook.com>
2021-12-08 06:42:15 +0100

    cleanup

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -5,7 +5,7 @@ import possibilityImage from '../../assets/possibility.png';
 const Possibility = () => (
 	<div className="gpt3__possibility section__padding" id="possibility">
 		<div className="gpt3__possibility-image">
-			<img src={possibilityImage} alt="possibility image" />
+			<img src={possibilityImage} alt="possibility" />
 		</div>
 		<div className="gpt3__possibility-content">
 			<h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
